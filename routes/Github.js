7aa2b757a00239dcbd0b52e1c247d49c6423cd5a
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-05-06 17:25:57 +0530

    customize option added

--- routes/Github.js
+++ routes/Github.js
@@ -3,33 +3,40 @@ let Github = require("../models/Github");
 
 router.route("/").get(async (req, res) => {
   const quaries = req.query;
-  const user = await Github.findOne({ userName: quaries["username"] });
+  const username = quaries["username"];
+  const style = quaries["style"] === undefined ? "flat" : quaries["style"];
+  const color = quaries["color"] === undefined ? "blue" : quaries["color"];
+
+  const user = await Github.findOne({ userName: username });
 
   if (user === null) {
     const newUser = new Github({
-      userName: quaries["username"],
+      userName: username,
       views: 1,
     });
     newUser
       .save()
       .then(() => {
         res.send(
-          `<img src = "https://img.shields.io/badge/Profile%20views-1-blue?style=flat" alt = "Profile views" />`
+          `<img src = "https://img.shields.io/badge/Profile%20views-1-${color}?style=${style}" alt = "Profile views" />`
         );
       })
       .catch(() => {
         res.send(
-          `<img src = "https://img.shields.io/badge/Profile%20views-Error-blue?style=flat" alt = "Profile views" />`
+          `<img src = "https://img.shields.io/badge/Profile%20views-Error-${color}?style=${style}" alt = "Profile views" />`
         );
       });
   } else {
     res.send(
-      `<img src = "https://img.shields.io/badge/Profile%20views-${user["views"] + 1}-blue?style=flat" alt = "Profile views" />`
+      `<img src = "https://img.shields.io/badge/Profile%20views-${
+        user["views"] + 1
+      }-${color}?style=${style}" alt = "Profile views" />`
     );
-    await Github.findOneAndUpdate({ userName: quaries["username"]} , {views : ++user["views"]})
+    await Github.findOneAndUpdate(
+      { userName: username },
+      { views: ++user["views"] }
+    );
   }
-
-
 });
 
 module.exports = router;
