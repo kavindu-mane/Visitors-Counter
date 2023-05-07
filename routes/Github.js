a0166326e3ab18e2c1bef31d138afa4cda2204b7
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-05-07 15:21:07 +0530

    response content type change

--- routes/Github.js
+++ routes/Github.js
@@ -41,7 +41,7 @@ router.route("/").get(async (req, res) => {
 async function sendingSvg(url, res) {
   let response = await axios.get(url);
   const svgXml = response.data;
-  res.set("Content-Type", "img/svg+xml");
+  res.set("Content-Type", "text/xml");
   res.send(svgXml);
 }
 
